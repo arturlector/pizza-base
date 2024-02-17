be610c9b8806d6405d50bedee26188aff9b51089
Artur Igberdin <art.igberdin@gmail.com>
2024-02-17 19:39:46 +0300

    PZL-3 Add Cart screen and Map screen

---
+++ pizza-base/CartController.swift
@@ -0,0 +1,18 @@
+//
+//  CartController.swift
+//  pizza-base
+//
+//  Created by Artur on 17.02.2024.
+//
+
+import UIKit
+
+final class CartController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+    
+}
