00a02cf97402d2d267bcd965ec898744c8b4862d
Artur Igberdin <art.igberdin@gmail.com>
2024-02-17 18:10:06 +0300

    PZL-1 Improve controller structure

--- pizza-base/MenuController.swift
+++ pizza-base/MenuController.swift
@@ -7,32 +7,38 @@
 
 import UIKit
 
-class MenuController: UIViewController {
+final class MenuController: UIViewController {
+    
+    //MARK: - UI
+    private var someButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .orange
+        button.setTitle("Детальная информация", for: .normal)
+        button.layer.cornerRadius = 8
+        button.clipsToBounds = true
+        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        button.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemPink
-        
-        let someButton = UIButton(type: .custom)
-        
-        someButton.backgroundColor = .orange
-        
-        someButton.setTitle("Детальная информация", for: .normal)
-        someButton.layer.cornerRadius = 8
-        someButton.clipsToBounds = true
-        someButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
-        
-        someButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
-        someButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(someButton)
-        
-        someButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
-        someButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
+        setupViews()
+        setupConstraints()
     }
-
-
 }
 
+//MARK: - Layout
+private extension MenuController {
+    func setupViews() {
+        view.backgroundColor = .systemPink
+        view.addSubview(someButton)
+    }
+    
+    func setupConstraints() {
+        someButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        someButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+}
--- pizza-base/SceneDelegate.swift
+++ pizza-base/SceneDelegate.swift
@@ -13,13 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let scene = (scene as? UIWindowScene) else { return }
-        
         window = UIWindow(windowScene: scene)
-        
         let menuVC = MenuController()
-        
         window?.rootViewController = menuVC
         window?.makeKeyAndVisible()
     }
