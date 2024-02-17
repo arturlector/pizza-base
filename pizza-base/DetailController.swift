01fff146bbcedf0e0ebc65e54df37986e42ad152
latush717 <132339929+latush717@users.noreply.github.com>
2024-02-17 18:23:02 +0300

    PZL-2 Develop Detail screen

---
+++ pizza-base/DetailController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailController.swift
+//  pizza-base
+//
+//  Created by latush on 17.02.2024.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    //MARK: - UI
+    private var someButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .orange
+        button.setTitle("Корзина", for: .normal)
+        button.layer.cornerRadius = 8
+        button.clipsToBounds = true
+        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        button.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupConstraints()
+    }
+}
+
+//MARK: - Layout
+private extension DetailController {
+    func setupViews() {
+        view.backgroundColor = .systemPink
+        view.addSubview(someButton)
+    }
+    
+    func setupConstraints() {
+        someButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        someButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+}
